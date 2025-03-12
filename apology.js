36428762a08ad966621c19e9ba28e825e275ba72
Devaansh Tiwari <tuf.gamer88@gmail.com>
2025-03-12 15:10:33 +0530

    Add files via upload

---
+++ apology.js
@@ -0,0 +1,4 @@
+function showLove() {
+    document.getElementById('response').innerHTML = "Yayayayayaya! I love you so much! 🥰🤗💖💖";
+    document.getElementById('response').style.display = "block";
+}
